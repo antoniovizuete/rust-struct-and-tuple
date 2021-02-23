e07d8472896dc1c6fad9509a4a9b6be2fda408e0
Antonio Vizuete <antoniovizuete@gmail.com>
2021-02-23 23:01:24 +0100

    Struct + impl version

--- src/main.rs
+++ src/main.rs
@@ -3,16 +3,18 @@ struct Rect {
     height: u32,
 }
 
+impl Rect {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+}
+
 fn main() {
     let rect = Rect {
         width: 14,
         height: 86,
     };
 
-    let area = area(&rect);
+    let area = rect.area();
     println!("area {}", area);
-}
-
-fn area(rect: &Rect) -> u32 {
-    rect.width * rect.height
 }
