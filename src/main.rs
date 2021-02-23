17fd8d54032a2dc82ef9d16be13eb9e7192700d4
Antonio Vizuete <antoniovizuete@gmail.com>
2021-02-23 22:56:11 +0100

    Tuple version

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,10 @@
 fn main() {
-    let width = 14;
-    let height = 86;
+    let rect: (u32, u32) = (14, 86);
 
-    let area = area(width, height);
+    let area = area(rect);
     println!("area {}", area);
 }
 
-fn area(width: u32, height: u32) -> u32 {
-    width * height
+fn area(rect: (u32, u32)) -> u32 {
+    rect.0 * rect.1
 }
